6f3aa79c85aa95d4651196f59adbb4f92e82218a
Shamilprog <shamilprog@yandex.ru>
2021-01-11 21:06:31 +0300

    Simple animation of objects' properties.

--- app/src/main/java/ru/shamilprog/android/sunset/SunsetFragment.java
+++ app/src/main/java/ru/shamilprog/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package ru.shamilprog.android.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +11,10 @@ import android.view.ViewGroup;
 
 public class SunsetFragment extends Fragment {
 
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -18,6 +23,29 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
