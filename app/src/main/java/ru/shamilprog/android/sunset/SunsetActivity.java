d46eb355ba11f599a7a30bd8ef5d376dc9209798
Shamilprog <shamilprog@yandex.ru>
2021-01-11 20:14:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/shamilprog/android/sunset/SunsetActivity.java
@@ -0,0 +1,13 @@
+package ru.shamilprog.android.sunset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunsetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sunset);
+    }
+}
